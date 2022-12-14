a8b2a4fed23348d9c52d47971145a7e9b7a325ed
hamitsehjal <hamitsehjal26@gmail.com>
2022-12-14 03:18:24 -0500

    Bcrypt working Fines!!

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import prisma from "./database"
-
+import bcrypt from "bcrypt"
 const express = require("express")
 const app = express()
 
@@ -15,14 +15,19 @@ app.post("/signup", async (req, res, next) => {
     const user = await prisma.user.findUnique({
         where: {
             name: req.body.name,
-            password: req.body.password
         }
     })
 
     if (user === null) {
+
+        // hash password
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        console.log(req.body.password)
+        console.log(hashedPassword)
         const user = await prisma.user.create({
             data: {
                 name: req.body.name,
+                password: hashedPassword
 
 
             }
@@ -31,30 +36,48 @@ app.post("/signup", async (req, res, next) => {
 
     }
     else {
+        res.status(400)
         res.json({ data: "User already Exist" })
     }
 
 
 
-    // try {
-
-    //     const user = await prisma.user.findUnique({
-    //         where: {
-    //             name: req.body.username
-    //         }
-    //     })
-    // } catch (err) {
-    //     return res.json({ errors: err })
-
-    // }
 
 })
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
+
+    const user = await prisma.user.findUnique({
+        where: {
+            name: req.body.name,
+        }
+    })
+
+    if (user === null) {
+        res.status(400)
+        return res.json({ data: "User DOES NOT Exist" })
+    }
+    else {
+        const hashedPassword = user.password;
+        console.log(req.body.password)
+        console.log(hashedPassword)
+        const isValid = await bcrypt.compare(req.body.password, hashedPassword)
+
+        if (isValid) {
+            res.status(200)
+            return res.json({ data: "You are LOGGED in" })
+        }
+        else {
+            res.status(400)
+            return res.json({ data: "Don't try to Trick Me" })
+        }
+
+    }
 
 })
 
 app.post("/logout", (req, res) => {
+    
 
 })
 
