236d9efe8860b645ea9ef8a1286e8c8d198dfebb
hamitsehjal <hamitsehjal26@gmail.com>
2022-12-14 04:21:45 -0500

    JWT-AccessToken implemented!!

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import prisma from "./database"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
 const express = require("express")
 const app = express()
 
@@ -7,7 +9,47 @@ require("dotenv").config()
 
 const PORT = process.env.PORT
 
+const authenticiate = async (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if (token === null) {
+        res.status(401)
+        return res.json({ message: "Sorry, you don't have access" })
+    }
+    else {
+        // we will verify the token
+        try {
+            const payload = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+            req.user = payload
+            console.log(payload)
+            res.status(200)
+            next()
+            return
+
+        } catch (err) {
+            res.status(401)
+            return res.json({ message: "Sorry, you don't have access" })
+        }
+    }
+}
+
 app.use(express.json())
+
+app.get("/users", authenticiate, async (req, res, next) => {
+    const users = await prisma.user.findMany();
+
+    // console.log(users)
+    const names = users.map(({ name }) => {
+        return {
+            name
+        }
+    })
+
+    console.log(names)
+    res.status(200)
+    return res.json({ data: names })
+})
 
 app.post("/signup", async (req, res, next) => {
     // we have to create a new user
@@ -65,7 +107,11 @@ app.post("/login", async (req, res) => {
 
         if (isValid) {
             res.status(200)
-            return res.json({ data: "You are LOGGED in" })
+            //create a JWT token
+            const token = generateAccessToken(user);
+
+            return res.json({ data: "You are LOGGED in", accesstoken: token })
+
         }
         else {
             res.status(400)
@@ -77,10 +123,14 @@ app.post("/login", async (req, res) => {
 })
 
 app.post("/logout", (req, res) => {
-    
+
 
 })
 
 app.listen(PORT, () => {
     console.log(`Express Server listening on Port: ${PORT}`)
 })
+
+const generateAccessToken = (user) => {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+}
